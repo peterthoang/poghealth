91214b398967f535a667d7fa81bf2bded2aad8ae
Peter Hoang <hoangpeter7@gmail.com>
2021-02-28 02:13:32 -0600

    heroku prep

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -23,6 +24,16 @@ mongoose
 
 //use routes
 app.use('/api/items', items);
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'intex.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
