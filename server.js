1f89ebe0ee12a99645d948028dbbaa64b9789f10
Peter Hoang <hoangpeter7@gmail.com>
2021-02-28 01:39:11 -0600

    added dotenv to protect mongodb uri

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const items = require('./routes/api/items');
 
